569cfe8b85b56f0a1717c9b4cc369bb4f5d4996c
Pikapro9531 <Magnus090627@gmail.com>
2025-09-11 16:10:40 +0200

    Kiosken
    Läs detta och ge mig C

--- if.py
+++ if.py
@@ -2,21 +2,35 @@ print("Hej, välkommen till den Koola kiosken!")
 print("Här kan du köpa Glass för 20kr styck, Varmkrov för 15kr styck, bröd kostar extra. Läsk för 15kr och sist en enstaka godis för 10kr")
 print("Så vad får det lov att vara?")
 item = input()
-Glass = 20
+icecream_price = 20
+Hotdog_price = 15
+Soda_price = 15
+Candy_price= 10
 
 if item == "Glass":
     print("Och hur mycket glass vill du ha?")
     amount = input()
     amount = int(amount)
+
+    print(icecream_price * amount, "kronor det att kosta")
+
 elif item == "Korv":
     print("Och hur många korvar vill du ha?")
     amount = input()
     amount = int(amount)
+
+    print(Hotdog_price * amount, "kronor kommer det att kosta")
+
 elif item == "Läsk":
     print("Och hur mycket läsk vill du ha?")
     amount = input()
     amount = int(amount)
+    
+    print(Soda_price * amount, "kronor kommer det att kosta")
+
 elif item == "Godis":
-    print("Och hur mycket läsk vill du ha?")
+    print("Och hur många Godisar vill du ha?")
     amount = input()
     amount = int(amount)
+    
+    print(Candy_price * amount, "kronor kommer det att kosta")
--- intro/Matte.py
+++ intro/Matte.py
@@ -1,3 +1,40 @@
-x = input()
-x = int(x)
-print((x*9/5)+32)
+
+def add(x, y):
+    return x+y
+
+def multiply(x, y):
+    return x*y
+
+def divide(x, y):
+    return x/y
+
+def subtrakt(x, y):
+    return x-y
+
+print("Välj 2 nummer du skulle vilha använda")
+number = input()
+number = int(number)
+
+print ("Och nu väljer du andra")
+number_2 = input()
+number_2 = int(number_2)
+
+print("Will du använda Addition, Multiplikation, Subtraktion eller Division?")
+
+word = input()
+
+if word == "Addition":
+    result = add(number, number_2)
+    print(result)
+
+elif word == "Subtraktion":
+    result = subtrakt(number, number_2) 
+    print (result)
+
+elif word == "Division":
+    result = divide(number, number_2)
+    print (result)
+
+elif word == "Multiplikation":
+    result = multiply(number, number_2)
+    print(result)
